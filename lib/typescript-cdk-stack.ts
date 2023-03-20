21f39be11f6776c7bb29646a924790d31b7131b0
Jota Freitas Jr <jotafreitasjr@gmail.com>
2023-03-20 11:42:50 -0300

    Adding a single S3 bucket
    Added a S3 bucket, need to install its dependency

--- lib/typescript-cdk-stack.ts
+++ lib/typescript-cdk-stack.ts
@@ -1,12 +1,14 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Bucket, BucketEncryption } from 'aws-cdk-lib/aws-s3';
 
 export class TypescriptCdkStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-    
+      const bucket = new Bucket(this, 'DocumentsBucket', {
+          encryption: BucketEncryption.S3_MANAGED
+      });
+
   }
 }
